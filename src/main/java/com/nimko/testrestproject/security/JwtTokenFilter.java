3fa86ee18936d79f38376c819f3880050dfe8966
Aleksandr Nimko <shurick2211@gmail.com>
2023-12-28 16:54:48 +0200

    swagger auth?

---
+++ src/main/java/com/nimko/testrestproject/security/JwtTokenFilter.java
@@ -0,0 +1,48 @@
+package com.nimko.testrestproject.security;
+
+import com.nimko.testrestproject.utils.JwtTokenUtils;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.security.SignatureException;
+import java.io.IOException;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class JwtTokenFilter extends OncePerRequestFilter{
+  private final JwtTokenUtils utils;
+
+  @Override
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+      FilterChain filterChain) throws ServletException, IOException {
+    String authHeader = request.getHeader("Authorization");
+    log.info(authHeader);
+    String username = null;
+    String jwt;
+    if (authHeader != null && authHeader.startsWith("Bearer ")) {
+      jwt = authHeader.substring(7);
+      try{
+        username = utils.getUserName(jwt);
+      } catch (ExpiredJwtException e) {
+        log.debug("Token lifetime period is out!");
+      } catch (SignatureException e) {
+        log.debug("Signature is wrong!");
+      }
+    }
+    if (username != null && SecurityContextHolder.getContext().getAuthentication() == null){
+      var token = new UsernamePasswordAuthenticationToken(
+          username,null);
+      SecurityContextHolder.getContext().setAuthentication(token);
+    }
+    filterChain.doFilter(request,response);
+  }
+}
--- src/main/java/com/nimko/testrestproject/security/SecurityConfig.java
+++ src/main/java/com/nimko/testrestproject/security/SecurityConfig.java
@@ -1,6 +1,8 @@
 package com.nimko.testrestproject.security;
 
 import com.nimko.testrestproject.services.PersonService;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,14 +18,17 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 @EnableGlobalMethodSecurity(securedEnabled = true)
+@SecurityScheme(name = "Token", scheme = "bearer" ,type = SecuritySchemeType.HTTP, bearerFormat = "JWT")
 public class SecurityConfig {
 
     private final PersonService personService;
+    private final JwtTokenFilter filter;
 
 
     @Bean
@@ -39,10 +44,9 @@ public class SecurityConfig {
                 .and()
                 .exceptionHandling()
                 .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
-//                .and()
-//                .httpBasic()
-                .and().build();
-
+                .and()
+                .addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class)
+                .build();
     }
 
     @Bean
